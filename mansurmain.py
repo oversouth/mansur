d279e90f2b2197b2221dadb314c3925dcd30a7c3
Pavel Zaikin <pavel.d.zaikin@gmail.com>
2026-09-06 14:19:19 +0400

    add mansur V0.1

---
+++ mansurmain.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/HelloWorld")
+def helloworld():
+    return "Hello World"
+
+
+@app.post("/new_user")
+def new_user(info: dict):
+    name = info.get("name")
+    pwd = info.get("password")
+
+    with open(name + ".txt", "w") as file:
+        file.write(f"name {name}\npassword {pwd}")
+
+    return {"message": "user created"}
+
+@app.post("/whoami")
+def whoami(info: dict):
+    name = info.get("name")
+
+    userprofile = localdb.get(name)
+
+    if userprofile:
+        return userprofile
+
+    return {"error": "user not found"}
